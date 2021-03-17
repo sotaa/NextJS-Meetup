ca845ac471446026a3b0a069bb4ff51f5478d5dc
soheil <soheilnajafimail2@gmail.com>
2021-03-17 22:12:22 +0330

    implement getServerSideProps, getStaticProps, getStaticPaths function

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,40 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        address: "Somestreet 25, 12345 San Somewhereo",
+        title: "A First Meetup",
+        description: "This is a first meetup!",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
